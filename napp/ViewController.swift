f5cddf864ee6fe2b627fa74810e583a6389a56c1
JNR122 <jonah.n.rubin@gmail.com>
2018-04-16 14:42:27 -0400

    Adding images, setting background color

--- napp/ViewController.swift
+++ napp/ViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eyeOpenView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "eyeOpen"))
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //view.backgroundColor = .blue
+        view.backgroundColor = UIColor(red: 51/255.0, green: 224/255.0, blue: 255/255.0, alpha: 1)
+
+        view.addSubview(eyeOpenView)
+
+        // enable autoLayout for imageView
+        eyeOpenView.translatesAutoresizingMaskIntoConstraints = false
+        eyeOpenView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        eyeOpenView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        eyeOpenView.widthAnchor.constraint(equalToConstant:  300).isActive = true
+        eyeOpenView.heightAnchor.constraint(equalToConstant :  200).isActive = true
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
